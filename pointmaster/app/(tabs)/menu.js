51d4a9e883479f416d8b6b384b7f08e71c9529bc
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-10-19 17:10:12 +0530

    changes on menu

--- pointmaster/app/(tabs)/menu.js
+++ pointmaster/app/(tabs)/menu.js
@@ -175,7 +175,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     paddingVertical: 10,
-
     backgroundColor: "#fff",
   },
   categoryButton: {
